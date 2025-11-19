ee8973433c795545959b7b9dcce4d49fd7f2a53c
ieltxuah <ieltxu.albin02@somo.eus>
2025-11-19 11:53:10 +0100

    Clase - Listado: Se crea la clase Listado para almacenar los objetos de la clase Videojuego

---
+++ Mini_Reto_Gestion_de_Tabla/Listado.vb
@@ -0,0 +1,27 @@
+﻿Public Class Listado
+    Private listado As ArrayList
+
+    Public Sub New()
+        listado = New ArrayList()
+    End Sub
+
+    Public Property PListado() As ArrayList
+        Get
+            Return listado
+        End Get
+        Set(ByVal value As ArrayList)
+            listado = value
+        End Set
+    End Property
+
+    Public Sub AniadirVideojuego(nuevoVideojuego As Videojuego)
+        listado.Add(nuevoVideojuego)
+    End Sub
+
+    Public Sub BorrarVideojuego(indice As Integer)
+        If indice < 0 OrElse indice >= listado.Count Then
+            Throw New ArgumentOutOfRangeException("Índice fuera de rango")
+        End If
+        listado.RemoveAt(indice)
+    End Sub
+End Class
