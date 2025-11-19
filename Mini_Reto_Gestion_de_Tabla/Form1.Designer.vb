5f0ceccddf1fd35f626665f62544f84857eedb8e
Arkaaiitz <arkaitz.jimenez06@somo.eus>
2025-11-19 12:38:42 +0100

    Form - Principal: Se crea diseño para la ventana principal Form1

--- Mini_Reto_Gestion_de_Tabla/Form1.Designer.vb
+++ Mini_Reto_Gestion_de_Tabla/Form1.Designer.vb
@@ -22,10 +22,139 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container()
-        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(800, 450)
-        Me.Text = "Form1"
+        dgvTablaVideojuegos = New DataGridView()
+        colTitVideojuego = New DataGridViewTextBoxColumn()
+        colPlataforma = New DataGridViewTextBoxColumn()
+        colEstudioDesarrollo = New DataGridViewTextBoxColumn()
+        colAnioEstreno = New DataGridViewTextBoxColumn()
+        colTiempoSpeedrun = New DataGridViewTextBoxColumn()
+        colRecordista = New DataGridViewTextBoxColumn()
+        colEliminar = New DataGridViewTextBoxColumn()
+        lbTitulo = New Label()
+        btAniadir = New Button()
+        btGuardar = New Button()
+        CType(dgvTablaVideojuegos, ComponentModel.ISupportInitialize).BeginInit()
+        SuspendLayout()
+        ' 
+        ' dgvTablaVideojuegos
+        ' 
+        dgvTablaVideojuegos.BackgroundColor = Color.Turquoise
+        dgvTablaVideojuegos.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        dgvTablaVideojuegos.Columns.AddRange(New DataGridViewColumn() {colTitVideojuego, colPlataforma, colEstudioDesarrollo, colAnioEstreno, colTiempoSpeedrun, colRecordista, colEliminar})
+        dgvTablaVideojuegos.Location = New Point(40, 104)
+        dgvTablaVideojuegos.MultiSelect = False
+        dgvTablaVideojuegos.Name = "dgvTablaVideojuegos"
+        dgvTablaVideojuegos.RowHeadersWidth = 51
+        dgvTablaVideojuegos.SelectionMode = DataGridViewSelectionMode.FullRowSelect
+        dgvTablaVideojuegos.Size = New Size(927, 406)
+        dgvTablaVideojuegos.TabIndex = 0
+        ' 
+        ' colTitVideojuego
+        ' 
+        colTitVideojuego.HeaderText = "Videjuego"
+        colTitVideojuego.MinimumWidth = 6
+        colTitVideojuego.Name = "colTitVideojuego"
+        colTitVideojuego.Width = 125
+        ' 
+        ' colPlataforma
+        ' 
+        colPlataforma.HeaderText = "Plataforma"
+        colPlataforma.MinimumWidth = 6
+        colPlataforma.Name = "colPlataforma"
+        colPlataforma.Width = 125
+        ' 
+        ' colEstudioDesarrollo
+        ' 
+        colEstudioDesarrollo.HeaderText = "Estudio de Desarrollo"
+        colEstudioDesarrollo.MinimumWidth = 6
+        colEstudioDesarrollo.Name = "colEstudioDesarrollo"
+        colEstudioDesarrollo.Width = 125
+        ' 
+        ' colAnioEstreno
+        ' 
+        colAnioEstreno.HeaderText = "Año de estreno"
+        colAnioEstreno.MinimumWidth = 6
+        colAnioEstreno.Name = "colAnioEstreno"
+        colAnioEstreno.Width = 125
+        ' 
+        ' colTiempoSpeedrun
+        ' 
+        colTiempoSpeedrun.HeaderText = "Tiempo de speedRun (s)"
+        colTiempoSpeedrun.MinimumWidth = 6
+        colTiempoSpeedrun.Name = "colTiempoSpeedrun"
+        colTiempoSpeedrun.Width = 125
+        ' 
+        ' colRecordista
+        ' 
+        colRecordista.HeaderText = "Nombre del Record"
+        colRecordista.MinimumWidth = 6
+        colRecordista.Name = "colRecordista"
+        colRecordista.Width = 125
+        ' 
+        ' colEliminar
+        ' 
+        colEliminar.HeaderText = "Eliminar"
+        colEliminar.MinimumWidth = 6
+        colEliminar.Name = "colEliminar"
+        colEliminar.Width = 125
+        ' 
+        ' lbTitulo
+        ' 
+        lbTitulo.AutoSize = True
+        lbTitulo.BorderStyle = BorderStyle.FixedSingle
+        lbTitulo.Font = New Font("Segoe UI", 18F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        lbTitulo.Location = New Point(394, 40)
+        lbTitulo.Name = "lbTitulo"
+        lbTitulo.Size = New Size(223, 43)
+        lbTitulo.TabIndex = 1
+        lbTitulo.Text = "VIDEOJUEGOS"
+        ' 
+        ' btAniadir
+        ' 
+        btAniadir.Font = New Font("Segoe UI Black", 9F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        btAniadir.Location = New Point(237, 555)
+        btAniadir.Name = "btAniadir"
+        btAniadir.Size = New Size(167, 48)
+        btAniadir.TabIndex = 2
+        btAniadir.Text = "AÑADIR"
+        btAniadir.UseVisualStyleBackColor = True
+        ' 
+        ' btGuardar
+        ' 
+        btGuardar.Font = New Font("Segoe UI Black", 9F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        btGuardar.Location = New Point(565, 560)
+        btGuardar.Name = "btGuardar"
+        btGuardar.Size = New Size(168, 43)
+        btGuardar.TabIndex = 3
+        btGuardar.Text = "GUARDAR"
+        btGuardar.UseVisualStyleBackColor = True
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(8F, 20F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(989, 634)
+        Controls.Add(btGuardar)
+        Controls.Add(btAniadir)
+        Controls.Add(lbTitulo)
+        Controls.Add(dgvTablaVideojuegos)
+        Name = "Form1"
+        Text = "Form1"
+        CType(dgvTablaVideojuegos, ComponentModel.ISupportInitialize).EndInit()
+        ResumeLayout(False)
+        PerformLayout()
     End Sub
+
+    Friend WithEvents dgvTablaVideojuegos As DataGridView
+    Friend WithEvents lbTitulo As Label
+    Friend WithEvents colTitVideojuego As DataGridViewTextBoxColumn
+    Friend WithEvents colPlataforma As DataGridViewTextBoxColumn
+    Friend WithEvents colEstudioDesarrollo As DataGridViewTextBoxColumn
+    Friend WithEvents colAnioEstreno As DataGridViewTextBoxColumn
+    Friend WithEvents colTiempoSpeedrun As DataGridViewTextBoxColumn
+    Friend WithEvents colRecordista As DataGridViewTextBoxColumn
+    Friend WithEvents colEliminar As DataGridViewTextBoxColumn
+    Friend WithEvents btAniadir As Button
+    Friend WithEvents btGuardar As Button
 
 End Class
--- Mini_Reto_Gestion_de_Tabla/Form1.vb
+++ Mini_Reto_Gestion_de_Tabla/Form1.vb
@@ -1,3 +1,21 @@
 ﻿Public Class Form1
 
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Dim btEliminar As New DataGridViewButtonColumn()
+        btEliminar.Name = "colEliminar"
+        btEliminar.HeaderText = "Eliminar"
+        btEliminar.Text = "Eliminar"
+        btEliminar.UseColumnTextForButtonValue = True
+        dgvTablaVideojuegos.Columns.Add(btEliminar)
+    End Sub
+
+    Private Sub dgvTablaVideojuegos_CellContentClick(sender As Object, e As DataGridViewCellEventArgs) Handles dgvTablaVideojuegos.CellContentClick
+        Dim filaSeleccionada As Integer = e.RowIndex
+
+        If (filaSeleccionada > 0 AndAlso dgvTablaVideojuegos.Columns(e.ColumnIndex).Name = "colEliminar") Then
+            dgvTablaVideojuegos.Rows.RemoveAt(filaSeleccionada)
+        End If
+
+    End Sub
 End Class
+
